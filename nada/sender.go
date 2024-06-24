6decdaac2d651f739d28b3a529b305a070551a98
Fabian Fromwald <fabian@fromwald.at>
2024-06-24 15:25:08 +0200

    Rtt has to be supplied by app

--- nada/sender.go
+++ nada/sender.go
@@ -4,7 +4,6 @@ import "time"
 
 type Sender struct {
 	refernceRate uint64 // Reference rate based on network congestion
-	rtt          uint64 // Estimated round-trip time
 	xPerv        uint64 // Previous value of aggregate congestion signal
 	lastReport   uint64 // in micro sec
 
@@ -16,22 +15,22 @@ func NewSender(config Config) Sender {
 
 	return Sender{
 		refernceRate: configPopulated.MinRate,
-		rtt:          0,
 		xPerv:        0,
 		config:       configPopulated,
 	}
 }
 
-func (s *Sender) FeedbackReport(xCurr uint64, recvRate uint64, rampUpMode bool) {
+// FeedbackReport calculates the new rate with the feedback from the receiver.
+// xCurr, recvRate, rampUpMode are from the reciver feedback.
+// rtt is the current rtt in micro seconds.
+func (s *Sender) FeedbackReport(xCurr uint64, recvRate uint64, rampUpMode bool, rtt uint64) {
 	currTime := uint64(time.Now().UnixMicro())
-
-	// TODO: update rtt
 
 	// measure feedback interval
 	delta := currTime - s.lastReport
 
 	if rampUpMode {
-		s.refernceRate = rampUpRate(*s.config, s.rtt, s.refernceRate, recvRate)
+		s.refernceRate = rampUpRate(*s.config, rtt, s.refernceRate, recvRate)
 	} else {
 		s.refernceRate = gradualUpdateRate(*s.config, s.refernceRate, xCurr, s.xPerv, delta)
 	}
